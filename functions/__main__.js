6510221cf8139667c4cb8417251b897fa32f27e2
Daniel Robinson <d7robin@gmail.com>
2018-05-13 23:04:46 +0000

    Error: Cannot read property '1' of undefined

--- functions/__main__.js
+++ functions/__main__.js
@@ -4,18 +4,9 @@ var util = require('util');
 
 /**
 * Your function call
-* @param {Object} params Execution parameters
-*   Members
-*   - {Array} args Arguments passed to function
-*   - {Object} kwargs Keyword arguments (key-value pairs) passed to function
-*   - {String} remoteAddress The IPv4 or IPv6 address of the caller
-*
-* @param {Function} callback Execute this to end the function call
-*   Arguments
-*   - {Error} error The error to show if function fails
-*   - {Any} returnValue JSON serializable (or Buffer) return value
 */
-module.exports = (params, callback) => {
+module.exports = (context, callback) => {
+    var params = context.params
     var formData = params.buffer && params.buffer.toString() || '';
     var formParams = formData && formData.length && qs.parse(formData) || {};
     var options = Object.assign({}, params.kwargs, formParams);
